901860d4cb41f8a03d20d270b893c9e162cc0d94
Vito Mule <mulevito@gmail.com>
2015-12-05 17:14:24 +0000

    CS50 week1

---
+++ junk/mario.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+  int height = 0;
+  while (height == 0 || height < 0 || height > 23) {
+    printf("Enter the eight of the pyramid:");
+    if ((scanf("%d", &height) == 0) || (height < 0 || height > 23)) {
+      fseek(stdin,0,SEEK_END);
+    }
+    else {
+      printf("Pyramid height is: %i\n", height);
+    }
+  }
+
+  for (int x = 1; x < height; x++) {
+    for (int j = 0; j < height; j++) {
+      if (j < height - x -1 ) {
+        printf(" ");
+      }
+      else {
+        printf("#");
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+}
+
