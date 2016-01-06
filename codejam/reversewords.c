5f5578a588b3639ec3b3f2e150a2a84d226d3e14
Vito Mule <mulevito@gmail.com>
2016-01-06 13:29:24 +0000

    use strncpy

--- codejam/reversewords.c
+++ codejam/reversewords.c
@@ -8,15 +8,14 @@ int main(void) {
 
   int i;
   int j = 0;
-  char word[1000][30];
+  char words[1000][30];
   char tmpword[1000];
   for (i = 0; i < N; i++) {
-    int getword = 1;
-    getword = scanf("%[a-z ]\n", tmpword);
+    scanf("%[a-z ]\n", tmpword);
     //printf("%s\n", tmpword);
     char *wordpointer = strtok(tmpword, " ");
     while (wordpointer != NULL) {
-      strcpy(word[j], wordpointer);
+      strncpy(words[j], wordpointer, strlen(wordpointer) + 1);
       j++;
       // a null pointer may be specified, in which case the function continues
       // scanning where a previous successful call to the function ended.
@@ -25,7 +24,7 @@ int main(void) {
     printf("Case #%i: ", (i+1));
 
     for (j = (j - 1) ; j >= 0; j--) {
-       printf("%s ", word[j]);
+       printf("%s ", words[j]);
     }
 
     j = 0;
