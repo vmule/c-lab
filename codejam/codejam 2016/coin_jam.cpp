f7cf64ce8b7f0f9fd17976e9df7bfc2a4151b06c
Vito Mule <mulevito@gmail.com>
2016-04-15 16:01:58 +0100

    fixes

--- codejam/codejam 2016/coin_jam.cpp
+++ codejam/codejam 2016/coin_jam.cpp
@@ -7,16 +7,8 @@
 #include <algorithm>
 
 unsigned long long isPrime(unsigned long long num) {
-  if ((num % 2) == 0 && num != 2) {
-   return 2;
-  }
-  for (unsigned long long i = 2; i <= sqrt(num); i += 2) {
-    if (i % 2 == 0) {
-      i++;
-    }
-    if ((num % i) == 0 && i != num) {
-      return i;
-    }
+  if  ((num % 3) == 0 || num == 0)  {
+    return 0;
   }
   return 1;
 }
@@ -34,52 +26,35 @@ int main(void) {
   }
   array[0] = 1;
   array[1] = 1;
+  array[N-1] = 1;
+  array[N-2] = 1;
 
-  unsigned long long base_array[11];
-  unsigned long long divisor_array[11];
   std::vector<unsigned long long> coins(J);
-  srand(time(NULL));
 
   std::cout << "Case #1:\n";
   int coins_count  = 0;
+  int isprime = 1;
   while (coins_count < J) {
-    int isprime = 1;
-    for (int i = 2; i < 11; i++) {
-      unsigned long long P = 0;
-      int pow_idx = (N - 1);
-      for (int z = 0; z < N; z++) {
-          P += (array[z] * (pow(i, pow_idx)));
-          pow_idx--;
-        }
-      unsigned long long  p = isPrime(P);
-      if (p > 1) {
-        base_array[i] = P;
-        divisor_array[i] = p;
-        P = 0;
-      } else {
-        isprime = 0;
-        P = 0;
-        break;
-      }
+    unsigned long long P = 0;
+    int pow_idx = (N - 1);
+    for (int z = 2; z < (N - 2); z++) {
+//      std::cout << P << "\n";
+      P += (array[z] * (long)pow(2, pow_idx));
+      pow_idx--;
     }
-    if (!isprime || (std::find(coins.begin(), coins.end(), base_array[10]) != coins.end())) {
-      isprime = 1;
-      int indx = rand() % (N-2) + 1;
+    unsigned long long  p = isPrime(P);
+    if (p || (std::find(coins.begin(), coins.end(), P) != coins.end())) {
+      int indx = rand() % (N-4) + 2;
       int value = rand() % 2;
       array[indx] = value;
-      continue;
     } else {
       for (int i = 0; i < N; i++) {
         std::cout << array[i];
       }
-      std::cout << " ";
-      for (int i = 2; i < 11; i++) {
-       std::cout << divisor_array[i]  << " ";
-      }
-      std::cout << "\n";
+      std::cout << " 3 4 5 6 7 8 9 10 11 \n";
       coins_count++;
-      coins.push_back(base_array[10]);
-      int indx = rand() % (N-2) + 1;
+      coins.push_back(P);
+      int indx = rand() % (N-4) + 2;
       int value = rand() % 2;
       array[indx] = value;
     }
