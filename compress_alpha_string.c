20206546f74cec634f1e0977fff6e8fc2c923839
Vito Mule <mulevito@gmail.com>
2016-05-19 15:39:40 +0100

    terminate strings correctly

--- compress_alpha_string.c
+++ compress_alpha_string.c
@@ -12,52 +12,62 @@ char* compress(char* str) {
         return str;
     }
 
-    for (i = 0; i < size_str; i++) {
-        if (i == 0) {
-            compress_str[z] = str[i];
-        } else {
-            if (str[i] == str[i-1]) {
+    compress_str[0] = str[0];
+    for (i = 1; i < size_str; i++) {
+      if (str[i] == str[i-1]) {
+          compress_str[z] = str[i];
+          if ( k >= 9 && k < 99) {
+          k++;
+          compress_str[z + 1] = (k / 10) + 48;
+          compress_str[z + 2] =  (k % 10) + 48;
+          } else if (k >= 99) {
+              k++;
+              compress_str[z + 1] = (k / 100) + 48;
+              compress_str[z + 2] =  ((k / 10) % 10) + 48;
+              compress_str[z + 3] =  (k % 10) + 48;
+          } else {
+              k++;
+              compress_str[z + 1] = k + 48;
+          }
+       } else {
+           if (k >= 10 && k < 100) {
+               z = z + 3;
+               k = 1;
                compress_str[z] = str[i];
-               if ( k >= 9 && k < 99) {
-               k++;
-               compress_str[z + 1] = (k / 10) + 48;
-               compress_str[z + 2] =  (k % 10) + 48;
-               } else if (k >= 99) {
-                   k++;
-                   compress_str[z + 1] = (k / 100) + 48;
-                   compress_str[z + 2] =  ((k / 10) % 10) + 48;
-                   compress_str[z + 3] =  (k % 10) + 48;
-               } else {
-                   k++;
-                   compress_str[z + 1] = k + 48;
-               }
-            } else {
-                if (k >= 10 && k < 100) {
-                    z = z + 3;
-                    k = 1;
-                    compress_str[z] = str[i];
-                } else if  (k >= 100) {
-                   z = z + 4;
-                   k = 1;
-                   compress_str[z] = str[i];
-                } else if (k > 1 && k <= 9) {
-                    z = z + 2;
-                    k = 1;
-                    compress_str[z] = str[i];
-                } else if (k == 1){
-                    z++;
-                    compress_str[z] = str[i];
-                }
-            }
-        }
+           } else if  (k >= 100) {
+              z = z + 4;
+              k = 1;
+              compress_str[z] = str[i];
+           } else if (k > 1 && k <= 9) {
+               z = z + 2;
+               k = 1;
+               compress_str[z] = str[i];
+           } else if (k == 1){
+               z++;
+               compress_str[z] = str[i];
+           }
+       }
    }
-   return compress_str;
+
+  if (k >= 10 && k < 100) {
+    z = z + 3;
+  } else if  (k >= 100) {
+    z = z + 4;
+  } else if (k > 1 && k <= 9) {
+    z = z + 2;
+  } else if (k == 1){
+    z++;
+  }
+
+  compress_str[z] = '\0';
+  return compress_str;
 }
 
 int main() {
     char* res;
     char* str;
     str = (char *)malloc(10240 * sizeof(char));
+    str[10240] = '\0';
     scanf("\n%[^\n]", str);
 
     res = compress(str);
