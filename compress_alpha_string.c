036afe88d4b9c54b6f173ec764016c019d3ca7c8
Vito Mule <mulevito@gmail.com>
2016-05-30 13:41:37 +0100

    compress string 2.0

--- compress_alpha_string.c
+++ compress_alpha_string.c
@@ -3,71 +3,55 @@
 #include <string.h>
 
 char* compress(char* str) {
-    int i;
-    int z = 0;
+    char* compress_str = (char*)calloc(1024, sizeof(char));
+    int i, j;
     int k = 1;
+    int z = 0;
     int size_str = strlen(str);
-    char *compress_str = malloc(size_str + 1);
-    if (size_str < 2) {
-        return str;
+
+    for (i = 0; i < size_str; i++) {
+      j = i;
+      while (str[i] == str[j+1]) {
+        k++;
+        j++;
+      }
+      if (k > 1) {
+        compress_str[z] = str[i];
+        z++;
+        if (k < 10) {
+          compress_str[z] = k  + 48;
+          z++;
+        } else if (k > 9 && k < 100) {
+          compress_str[z] = (k / 10) + 48;
+          z++; 
+          compress_str[z] = (k % 10) + 48;
+          z++;
+        } else if (k > 99) {
+          compress_str[z] = (k / 100) + 48;
+          z++;
+          compress_str[z] = ((k / 10) % 10) + 48;
+          z++;
+          compress_str[z] = (k % 10) + 48;
+          z++;
+        }
+        k = 1;
+        i = j;
+      } else {
+        compress_str[z] = str[i]; 
+        z++;
+      }
     }
-
-    compress_str[0] = str[0];
-    for (i = 1; i < size_str; i++) {
-      if (str[i] == str[i-1]) {
-          k++;
-          if ( k > 9 && k < 99) {
-          compress_str[z + 1] = (k / 10) + 48;
-          compress_str[z + 2] =  (k % 10) + 48;
-          } else if (k > 99) {
-              compress_str[z + 1] = (k / 100) + 48;
-              compress_str[z + 2] =  ((k / 10) % 10) + 48;
-              compress_str[z + 3] =  (k % 10) + 48;
-          } else {
-              compress_str[z + 1] = k + 48;
-          }
-       } else {
-           if (k > 9 && k < 100) {
-               z = z + 3;
-               k = 1;
-               compress_str[z] = str[i];
-           } else if  (k > 99) {
-              z = z + 4;
-              k = 1;
-              compress_str[z] = str[i];
-           } else if (k > 1 && k < 10) {
-               z = z + 2;
-               k = 1;
-               compress_str[z] = str[i];
-           } else if (k == 1){
-               z++;
-               compress_str[z] = str[i];
-           }
-       }
-   }
-
-  if (k > 9  && k < 100) {
-    z = z + 3;
-  } else if  (k > 99) {
-    z = z + 4;
-  } else if (k > 1 && k < 10) {
-    z = z + 2;
-  } else if (k == 1){
-    z++;
-  }
-
-  compress_str[z] = '\0';
-  return compress_str;
+    compress_str[z] = '\0';
+    return compress_str;
 }
 
-int main() {
-    char* res;
-    char* str;
-    str = (char *)malloc(10240 * sizeof(char));
-    str[10240] = '\0';
-    scanf("\n%[^\n]", str);
 
-    res = compress(str);
-    printf("%s\n", res);
-    return 0;
+int main(void) {
+  char* str = "zbbbcccccccccccccccdefgjjjjjnnnnn";
+  printf("Original string: %s\n", str);
+
+  char* compress_str = compress(str);
+  printf("Compressed string %s\n", compress_str);
+  return 0;
 }
+
