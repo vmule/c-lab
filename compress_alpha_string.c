1e8c3975ceb52b2cdb9bf464358fe33618807124
Vito Mule <mulevito@gmail.com>
2016-05-30 13:49:47 +0100

    better var naming

--- compress_alpha_string.c
+++ compress_alpha_string.c
@@ -5,43 +5,43 @@
 char* compress(char* str) {
     int size_str = strlen(str);
     char* compress_str = (char*)calloc((size_str+1), sizeof(char));
-    int i, j;
-    int k = 1;
-    int z = 0;
+    int i, tmp_i;
+    int rep_count = 1;
+    int cstr_i = 0;
 
     for (i = 0; i < size_str; i++) {
-      j = i;
-      while (str[i] == str[j+1]) {
-        k++;
-        j++;
+      tmp_i = i;
+      while (str[i] == str[tmp_i+1]) {
+        rep_count++;
+        tmp_i++;
       }
-      if (k > 1) {
-        compress_str[z] = str[i];
-        z++;
-        if (k < 10) {
-          compress_str[z] = k  + 48;
-          z++;
-        } else if (k > 9 && k < 100) {
-          compress_str[z] = (k / 10) + 48;
-          z++; 
-          compress_str[z] = (k % 10) + 48;
-          z++;
-        } else if (k > 99) {
-          compress_str[z] = (k / 100) + 48;
-          z++;
-          compress_str[z] = ((k / 10) % 10) + 48;
-          z++;
-          compress_str[z] = (k % 10) + 48;
-          z++;
+      if (rep_count > 1) {
+        compress_str[cstr_i] = str[i];
+        cstr_i++;
+        if (rep_count < 10) {
+          compress_str[cstr_i] = rep_count + 48;
+          cstr_i++;
+        } else if (rep_count > 9 && rep_count < 100) {
+          compress_str[cstr_i] = (rep_count / 10) + 48;
+          cstr_i++; 
+          compress_str[cstr_i] = (rep_count % 10) + 48;
+          cstr_i++;
+        } else if (rep_count > 99) {
+          compress_str[cstr_i] = (rep_count / 100) + 48;
+          cstr_i++;
+          compress_str[cstr_i] = ((rep_count / 10) % 10) + 48;
+          cstr_i++;
+          compress_str[cstr_i] = (rep_count % 10) + 48;
+          cstr_i++;
         }
-        k = 1;
-        i = j;
+        rep_count = 1;
+        i = tmp_i;
       } else {
-        compress_str[z] = str[i]; 
-        z++;
+        compress_str[cstr_i] = str[i]; 
+        cstr_i++;
       }
     }
-    compress_str[z] = '\0';
+    compress_str[cstr_i] = '\0';
     return compress_str;
 }
 
